1e33e87affbe791e056a2ae6004d7ff0661866f1
harshardhan <harshavardhan.harish@gmail.com>
2025-10-20 10:11:12 +0530

    initial update

--- API-GateWay/src/main/java/com/example/demo/ApiGateWayApplication.java
+++ API-GateWay/src/main/java/com/example/demo/ApiGateWayApplication.java
@@ -3,11 +3,8 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(
-    exclude = {
-        org.springframework.boot.actuate.autoconfigure.security.reactive.ReactiveManagementWebSecurityAutoConfiguration.class
-    }
-)
+@SpringBootApplication    
+
 public class ApiGateWayApplication {
 
     public static void main(String[] args) {
