b727a30419e7c4ad56774e46519f6fa2b38322aa
harshardhan <harshavardhan.harish@gmail.com>
2025-10-28 12:49:35 +0530

    initial update

--- API-GateWay/src/main/java/com/example/demo/SecurityConfig.java
+++ API-GateWay/src/main/java/com/example/demo/SecurityConfig.java
@@ -32,12 +32,14 @@ public class SecurityConfig {
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         http.authorizeHttpRequests(auth -> auth
-                .requestMatchers(HttpMethod.POST,"/api/auth/**").permitAll()  // ✅ allow ALL auth endpoints
+                .requestMatchers(HttpMethod.POST,"/api/auth/login*").permitAll()  // ✅ allow ALL auth endpoints
                 .requestMatchers(HttpMethod.POST, "/api/users/register").permitAll() // ✅ public register
                 .requestMatchers("/actuator/**").permitAll() // ✅ Allow healthcheck
                 .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() // ✅ CORS
                 .anyRequest().authenticated()
         );
+        // ✅ THIS WAS MISSING — JWT FILTER REGISTRATION
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
 
         return http.build();
