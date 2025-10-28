0c91af01adbee516fdbdd874cf00117443c7e0c3
harshardhan <harshavardhan.harish@gmail.com>
2025-10-28 12:26:06 +0530

    initial update

--- API-GateWay/src/main/java/com/example/demo/SecurityConfig.java
+++ API-GateWay/src/main/java/com/example/demo/SecurityConfig.java
@@ -32,8 +32,8 @@ public class SecurityConfig {
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         http.authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/auth/**").permitAll()  // ✅ allow ALL auth endpoints
-                .requestMatchers("/api/users/register").permitAll() // ✅ public register
+                .requestMatchers(HttpMethod.POST,"/api/auth/**").permitAll()  // ✅ allow ALL auth endpoints
+                .requestMatchers(HttpMethod.POST, "/api/users/register").permitAll() // ✅ public register
                 .requestMatchers("/actuator/**").permitAll() // ✅ Allow healthcheck
                 .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() // ✅ CORS
                 .anyRequest().authenticated()
