1e61c5cbe82344d75e98079d5337ecc67103e694
Harshavardhan <harshavardhan.harish@gmail.com>
2025-12-13 15:57:25 +0530

    Rename API-GatewayApplication to API-Gateway

--- API-Gateway/src/main/java/com/example/demo/API-Gateway.java
+++ API-Gateway/src/main/java/com/example/demo/API-Gateway.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class API-GatewayApplication {
+public class API-Gateway {
 
 	public static void main(String[] args) {
-		SpringApplication.run(API-GatewayApplication.class, args);
+		SpringApplication.run(API-Gateway.class, args);
 	}
 
 }
